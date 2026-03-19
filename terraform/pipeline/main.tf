e4e355a1d2c29ff63448bccbcef461289489d0d6
Christina Stiller <christina.stiller@outlook.com>
2026-03-19 19:38:02 +0100

    Fix version mismatch in TF lock file to avoid pipeline error, set pessimistic constraits to allow minor updates instead of patch updates, Closes #16

--- terraform/pipeline/main.tf
+++ terraform/pipeline/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.14.0"
+  required_version = "~> 1.14"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 7.18.0"
+      version = "~> 7.18"
     }
   }
 
